3f5d223e8a7cce88b34690a64f2ea3a41dbad128
jin zhang <jinzhang7788@hotmail.com>
2022-07-29 13:16:42 -0600

    Finish basic function of prework

---
+++ PreWork/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  PreWork
+//
+//  Created by Jin Zhang on 7/29/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+
+    @IBAction func rates(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
